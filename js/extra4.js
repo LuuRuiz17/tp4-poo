552dcde0a03404969428034e648e85220d497a4e
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-04 19:06:56 -0300

    Realicé el último ejercicio de Taiga

--- js/extra4.js
+++ js/extra4.js
@@ -10,39 +10,50 @@ function addGrades(newAlumn) {
     } while (confirm("¿Desea agregar otra nota?"))
 }
 
-function findBestGrade(students){
+function findBestGrade(students) {
     let maxGrade = 0;
-    for(let i = 0; i < students.length; i++){
-        for(let j = 0; j < students[i].grades.length; j++){
-            if(students[i].grades[j] > maxGrade){
+    for (let i = 0; i < students.length; i++) {
+        for (let j = 0; j < students[i].grades.length; j++) {
+            if (students[i].grades[j] > maxGrade) {
                 maxGrade = students[i].grades[j];
             }
         }
-    } 
+    }
     console.log(`Nota más alta: ${maxGrade}`);
 
-    switch(maxGrade){
+    switch (maxGrade) {
         case 0:
         case 1:
         case 2:
         case 3:
         case 4:
-            document.writeln(`<p>Calificación más alta: ${maxGrade} (D)</p>`)
+            return maxGrade + " (D)"
             break;
         case 5:
         case 6:
-            document.writeln(`<p>Calificación más alta: ${maxGrade} (C)</p>`)
+            return maxGrade + " (C)"
             break;
         case 7:
         case 8:
-            document.writeln(`<p>Calificación más alta: ${maxGrade} (B)</p>`)
+            return maxGrade + " (B)"
             break;
         case 9:
         case 10:
-            document.writeln(`<p>Calificación más alta: ${maxGrade} (A)</p>`)
+            return maxGrade + " (A)"
             break;
 
     }
+}
+
+function countGrades(students){
+    let maxNGrades = 0;
+    for(let i = 0; i < students.length; i++){
+        if(students[i].grades.length > maxNGrades){
+            maxNGrades = students[i].grades.length
+        }
+    }
+    console.log(`Cantidad máxima de notas: ${maxNGrades}`);
+    return maxNGrades;
 }
 
 const students = [];
@@ -59,19 +70,47 @@ do {
 
 } while (confirm("¿Querés agregar otro alumno?"));
 
+document.writeln(`<table class="table table-responsive w-75 mt-3 m-auto style="table-layout: fixed">
+    <thead>
+        <tr>
+            <th class="bg-primary-subtle text-center border border-1 border-dark tableTitle">Nombre</th>
+            <th class="bg-primary-subtle text-center border border-1 border-dark tableTitle">Curso</th>
+            <th class="bg-primary-subtle text-center border border-1 border-dark w-50">Calificaciones</th>
+            <th class="bg-primary-subtle text-center border border-1 border-dark tableTitle">Clasificación</th>
+        </tr>
+    </thead>
+    <tbody>
+`);
+
 for (let i = 0; i < students.length; i++) {
-    document.writeln(`<p>Nombre del alumno: ${students[i].nombre}</p>`);
-    document.writeln(`<p>Curso: ${students[i].curso}</p>`);
-    document.writeln(`<p>Calificaciones:`);
-    for (let j = 0; j < students[i].grades.length; j++) {
-        if (j != students[i].grades.length - 1) {
-            document.writeln(`${students[i].grades[j]} - `)
+    document.writeln(`<tr>`);
+
+    document.writeln(`<td class="border-1 border-dark text-center">${students[i].nombre}</td>`);
+    document.writeln(`<td class="border-1 border-dark text-center">${students[i].curso}</td>`);
+    document.writeln(`<td class="border-1 border-dark p-0 text-center">`);
+    document.writeln(`<table class="w-100 m-0" style="table-layout: fixed;"><tr>`);
+
+    for (let j = 0; j < countGrades(students); j++) {
+        if (students[i].grades[j]) {
+            if(j != countGrades(students) - 1){
+                document.writeln(`<td class="border-end border-dark text-center p-2">${students[i].grades[j]}</td>`);
+            }else{
+                document.writeln(`<td class="text-center p-2">${students[i].grades[j]}</td>`);
+            }
         } else {
-            document.writeln(`${students[i].grades[j]}`);
+            document.writeln(`<td class="border-end border-dark text-center p-2"> - </td>`);
         }
     }
-    document.writeln(`</p>`)
+    
+
+    document.writeln(`</tr></table>`);
+    document.writeln(`</td>`);
+    document.writeln(`<td class="border-1 border-dark text-center">${findBestGrade(students)}</td>`);
+    document.writeln(`</tr>`); 
 }
+
+document.writeln(`</tbody></table>`);
+
 
 findBestGrade(students);
 
